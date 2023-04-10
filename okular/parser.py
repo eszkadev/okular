ea0b6edef3ecb4185ced2d8b74dedb9e3fda552b
Szymon Kłos <eszkadev@gmail.com>
2023-04-10 20:16:57 +0200

    Detect resource error

--- okular/parser.py
+++ okular/parser.py
@@ -9,6 +9,7 @@ class Parser:
     def parse(self):
         cypress_fail_prefix = 'Cypress test failed: '
         unittest_fail_prefix = 'Test failed on '
+        write_error_prefix = 'write error: Resource temporarily unavailable'
         for line in self.input.split('\n'):
             if line.find(cypress_fail_prefix) != -1:
                 test_name = line[len(cypress_fail_prefix):]
@@ -16,6 +17,8 @@ class Parser:
             if line.find(unittest_fail_prefix) != -1:
                 test_name = line[len(unittest_fail_prefix):].split(' ')[0]
                 self.fails.append(test_name)
+            if line.find(write_error_prefix) != -1:
+                self.fails.append('write error: Resource temporarily unavailable')
 
     def get_fails(self):
         return self.fails
