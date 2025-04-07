a669ed432d789125bf27f2333316487a57273ef7
Szymon Kłos <eszkadev@gmail.com>
2025-04-07 21:22:31 +0200

    Add success rate counter
    Signed-off-by: Szymon Kłos <eszkadev@gmail.com>

--- okular/viewmodels/job_dashboard.py
+++ okular/viewmodels/job_dashboard.py
@@ -9,6 +9,9 @@ from sqlalchemy import func
 class JobDashboardViewModel(BaseViewModel):
     jenkins_job: str
     fails: []
+    build_fails_count: int
+    builds_count: int
+    success_rate: str
 
     def __init__(self, jenkins_api, last_update_str, jenkins_job):
         self.jenkins_api = jenkins_api
@@ -18,13 +21,29 @@ class JobDashboardViewModel(BaseViewModel):
 
         last_week_date = date.today() + timedelta(days=-7)
         build_fails = (dbcontext.session.query(Builds).join(BuildFails)
-                       .with_entities(Builds.id, BuildFails.test_name, func.count(BuildFails.test_name))
-                       .where(BuildFails.test_name != '')
-                       .filter(Builds.date.between(last_week_date, date.today()))
-                       .group_by(BuildFails.test_name)
-                       .order_by(func.count(BuildFails.test_name).desc())
-                       .limit(10)
-                       .all())
+                        .with_entities(Builds.id, BuildFails.test_name, func.count(BuildFails.test_name))
+                        .where(BuildFails.test_name != '')
+                        .filter(Builds.date.between(last_week_date, date.today()))
+                        .group_by(BuildFails.test_name)
+                        .order_by(func.count(BuildFails.test_name).desc())
+                        .limit(10)
+                        .all())
+
+        self.build_fails_count = (dbcontext.session.query(Builds).join(BuildFails)
+                        .with_entities(Builds.id, BuildFails.test_name, func.count(BuildFails.test_name))
+                        .where(BuildFails.test_name != '')
+                        .filter(Builds.date.between(last_week_date, date.today()))
+                        .group_by(Builds.id)
+                        .count())
+
+        self.builds_count = (dbcontext.session.query(Builds)
+                        .filter(Builds.date.between(last_week_date, date.today()))
+                        .count())
+
+        if self.builds_count > 0:
+            self.success_rate = str(int(100 - self.build_fails_count / self.builds_count * 100)) + '%'
+        else:
+            self.success_rate = 'N/A'
 
         self.fails = []
         for fail in build_fails:
